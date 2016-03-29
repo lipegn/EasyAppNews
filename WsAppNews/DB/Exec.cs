41a8749acdcf23276ccbdd99c3a61099ec93a96d
lipegn <felipe.goncalves@ipsos.com>
2016-03-29 15:16:08 -0300

    d

---
+++ WsAppNews/DB/Exec.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WsAppNews.DB
+{
+    public static class Exec
+    {
+        public static EasyAppNewsEntities RetornaDb()
+        {
+            try
+            {
+                return new EasyAppNewsEntities(new dllAcessoWeb.clsAcessoBanco("EasyAppNews").RetornaConexao());
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
